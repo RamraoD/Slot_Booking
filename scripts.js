443171ec8db8a4a481adfbb5e9952bbf56652858
RamraoD <ramraodesai009@gmail.com>
2020-08-25 17:16:27 +0530

    created web page of slot booking

---
+++ scripts.js
@@ -0,0 +1,55 @@
+let days = [];
+let daysRequired = 6
+
+for (let i = 0; i <= daysRequired; i++) {
+    days.push(moment().add(i, 'days').format('dddd, Do MMMM YYYY'))
+}
+$(document).ready(function () {
+    var day1 = days[0]
+    $('#lista').html(
+        $('<div/>', {
+            html: day1
+        }).text()
+    )
+    var day2 = days[1]
+    $('#listb').html(
+        $('<div/>', {
+            html: day2
+        }).text()
+    )
+    var day3 = days[2]
+    $('#listc').html(
+        $('<div/>', {
+            html: day3
+        }).text()
+    )
+    var day4 = days[3]
+    $('#listd').html(
+        $('<div/>', {
+            html: day4
+        }).text()
+    )
+    var day5 = days[4]
+    $('#liste').html(
+        $('<div/>', {
+            html: day5
+        }).text()
+    )
+    var day6 = days[5]
+    $('#listf').html(
+        $('<div/>', {
+            html: day6
+        }).text()
+    )
+    var day7 = days[6]
+    $('#listg').html(
+        $('<div/>', {
+            html: day7
+        }).text()
+    )
+});
+
+$('.cssbuttongo').click(function () {
+    $('#myModal').modal('show');
+});
+
