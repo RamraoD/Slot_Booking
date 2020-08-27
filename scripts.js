a03baaa43c09c43b482fd29dc473aaba3781df10
RamraoD <ramraodesai009@gmail.com>
2020-08-27 15:08:47 +0530

    updated datepick according to calender select in fe

--- scripts.js
+++ scripts.js
@@ -3,44 +3,66 @@ $(document).ready(function () {
     onSelect: function (_date, _datepicker) {
       var currentDate = new Date(_date);
       var next7Days = [];
-
-      for (var i = 0; i < 7; i++) {
-        var nextDate = moment(currentDate).add(i + 1, "days");
-        next7Days.push(nextDate);
-        console.log(nextDate.format("dddd, Do MMMM YYYY"));
+      let daysRequired = 7;
+      for (var i = 0; i < daysRequired; i++) {
+        // var nextDate = moment(currentDate).add(i + 1, "days");
+        // next7Days.push(nextDate);
+        // console.log(nextDate.format("ddd, Do MMM YYYY"));
+        next7Days.push(moment(currentDate).add(i + 1, "days").format("dddd, Do MMMM YYYY"));
+        console.log(next7Days)
       }
-
+      var day1 = next7Days[0]
+      $('#lista').html(
+          $('<div/>', {
+              html: day1
+          }).text()
+      )
+      var day2 = next7Days[1]
+      $('#listb').html(
+          $('<div/>', {
+              html: day2
+          }).text()
+      )
+      var day3 = next7Days[2]
+      $('#listc').html(
+          $('<div/>', {
+              html: day3
+          }).text()
+      )
+      var day4 = next7Days[3]
+      $('#listd').html(
+          $('<div/>', {
+              html: day4
+          }).text()
+      )
+      var day5 = next7Days[4]
+      $('#liste').html(
+          $('<div/>', {
+              html: day5
+          }).text()
+      )
+      var day6 = next7Days[5]
+      $('#listf').html(
+          $('<div/>', {
+              html: day6
+          }).text()
+      )
+      var day7 = next7Days[6]
+      $('#listg').html(
+          $('<div/>', {
+              html: day7
+          }).text()
+      )
       return true;
-      // console.log(myDate)
-      myDate.setDate(myDate.getDate() + 6);
-      var daysOfYear = 6;
-      for (var d = new Date(_date); d <= myDate; d.setDate(d.getDate() + 6)) {
-        console.log(myDate);
-        daysOfYear.push(
-          (d.getMonth() > 8 ? d.getMonth() + 1 : "0" + (d.getMonth() + 1)) +
-            "/" +
-            (d.getDate() > 9 ? d.getDate() : "0" + d.getDate()) +
-            "/" +
-            d.getFullYear()
-        );
-        console.log(d);
-      }
-      console.log(daysOfYear);
-      var day1 = daysOfYear[0];
-      $(".list").html(
-        $("<div/>", {
-          html: day1,
-        }).text()
-      );
     },
   });
 });
 
-function myFunction() {
-  let days = [];
-  let daysRequired = 6;
-  for (let i = 0; i <= daysRequired; i++) {
-    days.push(moment().add(i, "days").format("dddd, Do MMMM YYYY"));
-  }
-  console.log(days);
-}
+// function myFunction() {
+//   let days = [];
+//   let daysRequired = 6;
+//   for (let i = 0; i <= daysRequired; i++) {
+//     days.push(moment().add(i, "days").format("dddd, Do MMMM YYYY"));
+//   }
+//   console.log(days);
+// }
