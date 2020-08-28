d2e3bd5722a5fbe9ce3b451fbcb1fc0070090c0f
RamraoD <ramraodesai009@gmail.com>
2020-08-28 22:29:31 +0530

    created buttons

--- scripts.js
+++ scripts.js
@@ -2,62 +2,64 @@ $(document).ready(function () {
   $("#datepicker").datepicker({
     onSelect: function (_date, _datepicker) {
       var currentDate = new Date(_date);
-      var timeslot =[];
+      var timeslot = [];
       var next7Days = [];
       let daysRequired = 7;
       for (var i = 0; i < daysRequired; i++) {
         // var nextDate = moment(currentDate).add(i + 1, "days");
         // next7Days.push(nextDate);
         // console.log(nextDate.format("ddd, Do MMM YYYY"));
-        next7Days.push(moment(currentDate).add(i + 1, "days").format("dddd, Do MMMM YYYY"));
+        next7Days.push(moment(currentDate).add(i, "days").format("ddd, Do MM YYYY"));
         // console.log(next7Days)
-      }
-      var day1 = next7Days[0]
-      $('#lista').html(
+      } 
+      // for (var i=0;i< next7Days.length;i++) {
+        var list = next7Days[0]
+        $('#day1').html(
           $('<div/>', {
-              html: day1
+            html: list
           }).text()
-      )
-      var day2 = next7Days[1]
-      $('#listb').html(
+        )
+        var list = next7Days[1]
+        $('#day2').html(
           $('<div/>', {
-              html: day2
+            html: list
           }).text()
-      )
-      var day3 = next7Days[2]
-      $('#listc').html(
+        )
+        var list = next7Days[2]
+        $('#day3').html(
           $('<div/>', {
-              html: day3
+            html: list
           }).text()
-      )
-      var day4 = next7Days[3]
-      $('#listd').html(
+        )
+        var list = next7Days[3]
+        $('#day4').html(
           $('<div/>', {
-              html: day4
+            html: list
           }).text()
-      )
-      var day5 = next7Days[4]
-      $('#liste').html(
+        )
+        var list = next7Days[4]
+        $('#day5').html(
           $('<div/>', {
-              html: day5
+            html: list
           }).text()
-      )
-      var day6 = next7Days[5]
-      $('#listf').html(
+        )
+        var list = next7Days[5]
+        $('#day6').html(
           $('<div/>', {
-              html: day6
+            html: list
           }).text()
-      )
-      var day7 = next7Days[6]
-      $('#listg').html(
+        )
+        var list = next7Days[6]
+        $('#day7').html(
           $('<div/>', {
-              html: day7
+            html: list
           }).text()
-      )
+        )
+      // }
       return true;
     },
   });
-//   timeslot function
+  //   timeslot function
   function getTimeSlot(start, end){
     var startTime = moment(start, 'HH:mm');
     var endTime = moment(end, 'HH:mm');
@@ -67,7 +69,7 @@ $(document).ready(function () {
     }
   
     var timeSlot = [];
-  
+
     while(startTime <= endTime){
         timeSlot.push(new moment(startTime).format('HH:mm'));
       startTime.add(60, 'minutes');
@@ -76,28 +78,23 @@ $(document).ready(function () {
   }
   var timeSlot = getTimeSlot('09:00', '18:00');
   console.log('timeSlot ', timeSlot);
-
-//   Creating Dynamic buttons
-    var $btn1=$('<input/>').attr({type:'button',name:'mybutton',value: timeSlot[0]});
-    var $btn2=$('<input/>').attr({type:'button',name:'mybutton',value:timeSlot[1]});
-    var $btn3=$('<input/>').attr({type:'button',name:'mybutton',value:timeSlot[2]});
-    var $btn4=$('<input/>').attr({type:'button',name:'mybutton',value:timeSlot[3]});
-    var $btn5=$('<input/>').attr({type:'button',name:'mybutton',value:timeSlot[4]});
-    var $btn6=$('<input/>').attr({type:'button',name:'mybutton',value:timeSlot[5]});
-    var $btn7=$('<input/>').attr({type:'button',name:'mybutton',value:timeSlot[6]});
-    var $btn8=$('<input/>').attr({type:'button',name:'mybutton',value:timeSlot[7]});
-    var $btn9=$('<input/>').attr({type:'button',name:'mybutton',value:timeSlot[8]});
-    var $btn10=$('<input/>').attr({type:'button',name:'mybutton',value:timeSlot[9]});
-    $("#div1").append($btn1)
-    $("#div2").append($btn2)
-    $("#div3").append($btn3)
-    $("#div4").append($btn4)
-    $("#div5").append($btn5)
-    $("#div6").append($btn6)
-    $("#div7").append($btn7)
-    $("#div8").append($btn8)
-    $("#div9").append($btn9)
-    $("#div10").append($btn10)
+  
+  for(var i=0;i<timeSlot.length;i++) {
+    var $btnOne=$('<input/>').attr({type:'button',value: timeSlot[i]});
+    var $btnTwo=$('<input/>').attr({type:'button',value: timeSlot[i]});
+    var $btnThree=$('<input/>').attr({type:'button',value: timeSlot[i]});
+    var $btnFour=$('<input/>').attr({type:'button',value: timeSlot[i]});
+    var $btnFive=$('<input/>').attr({type:'button',value: timeSlot[i]});
+    var $btnSix=$('<input/>').attr({type:'button',value: timeSlot[i]});
+    var $btnSeveen=$('<input/>').attr({type:'button',value: timeSlot[i]});
+    $("#btnOne").append($btnOne)
+    $("#btnTwo").append($btnTwo)
+    $("#btnThree").append($btnThree)
+    $("#btnFour").append($btnFour)
+    $("#btnFive").append($btnFive)
+    $("#btnSix").append($btnSix)
+    $("#btnSeveen").append($btnSeveen)
+  }
 });
 
 
